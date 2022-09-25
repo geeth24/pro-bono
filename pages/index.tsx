7c8809bbf65956c7be6f2d4b237247a240edf248
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-09-24 20:39:59 -0500

    Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next"
+import Head from "next/head"
 import { Text } from "@mantine/core"
 
-
 const Home: NextPage = () => {
-  return (
-   <>
-   <Head>
-
-   </Head>
-   <Text size="xl">Hello World</Text>
-   </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Legal Dove</title>
+            </Head>
+            <Text size="xl">Hello World</Text>
+        </>
+    )
 }
 
 export default Home
